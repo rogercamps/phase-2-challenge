51c01c26561f154b2cda9fe9f8ae66cd76609f39
rogercamps <rogercamps@me.com>
2017-06-19 23:34:40 -0700

    setup folders for the 3 parts of assignment

---
+++ part-1/weekday.js
@@ -0,0 +1,18 @@
+// weekday(new Date(2017, 5, 15))
+// create function weekday(date)
+// ha de tenir els dies de la setmana en un variable
+// a traves duna data poder trobar quin dia de la setmana
+
+function weekday(date) {
+  let days = [sun,mon,tue,wed,thu,fri,sat]
+
+}
+
+
+// let date = new Date(2017, 5, 19)
+// weekday(date)
+
+Date.getDay()
+
+var dt = new Date("December 25, 1995 23:15:00");
+document.write("getDay() : " + dt.getDay() ); 
